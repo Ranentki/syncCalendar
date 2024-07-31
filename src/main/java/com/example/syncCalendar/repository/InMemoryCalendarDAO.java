4b9835f3c383b6b47ecc42d36196f35fdcbcf6bc
alina <topperalina13@gmail.com>
2024-07-31 20:56:05 +0300

    new level - repository for work with data

---
+++ src/main/java/com/example/syncCalendar/repository/InMemoryCalendarDAO.java
@@ -0,0 +1,10 @@
+package com.example.syncCalendar.repository;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class InMemoryCalendarDAO {
+
+    private List<Calendar> Days = new ArrayList<>();
+}
