dfe59a688ee658913440f2ba28ba7fc50dd05712
Roman Ivashkin <st086961@student.spbu.ru>
2024-08-06 11:53:40 +0300

    Remove comment from migration

--- src/main/resources/db/migration/V1__CalendarTable.sql
+++ src/main/resources/db/migration/V1__CalendarTable.sql
@@ -1,6 +1,3 @@
--- Миграции нужны для создания таблицы в базе данных,
--- а также изменения структуры существующей таблицы
--- при изменения формы данных.
 CREATE TABLE calendar (
     day INT NOT NULL PRIMARY KEY,
     description VARCHAR(255) NOT NULL
